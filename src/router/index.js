8f58ef8b1357127e70b28ef9a0f884f4011bc8ad
zjhch123 <416836508@qq.com>
2017-10-03 10:15:27 +0800

    feat: 增加路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,33 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
+      path: '/launch',
+      name: 'launch',
+      component: Hello
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: Hello
+    },
+    {
+      path: '/photo',
+      name: 'photo',
+      component: Hello
+    },
+    {
+      path: '/savePhoto',
+      name: 'savePhoto',
+      component: Hello
+    },
+    {
+      path: '/sharePhoto',
+      name: 'sharePhoto',
+      component: Hello
+    },
+    {
+      path: '/cloud',
+      name: 'cloudTakePhoto',
       component: Hello
     }
   ]
