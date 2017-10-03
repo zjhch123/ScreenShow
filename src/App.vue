7e9ff3ec91cb882d2c36420f7cfadbb4d432534c
zjhch123 <416836508@qq.com>
2017-10-03 19:58:51 +0800

    feat: p1, p2完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <img src="./assets/bg.png" class="u-app-bg"/>
+    <div class="c-app-container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -10,14 +12,37 @@ export default {
   name: 'app'
 }
 </script>
-<style>
+<style lang="scss">
+html, body {
+  width: 680px;
+  height: 1210px;
+  margin: auto;
+}
 #app {
-  font-family: '微软雅黑', 'Avenir', Helvetica, Arial, sans-serif;
+  background-color: black;
+  font-family: '微软雅黑';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 680px;
+  height: 1210px;
+  position: relative;
+  overflow: hidden;
+  .u-app-bg {
+    width: 680px;
+    height: 1210px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    z-index: 0;
+  }
+  .c-app-container {
+    position: relative;
+    z-index: 2;
+    height: 100%;
+    width: 100%;
+  }
 }
 body, dl, dd, ul, ol, h1, h2, h3, h4, h5, h6, p, form, input, textarea, button, th, td { margin: 0; padding: 0; }
 img, iframe { border: 0 none; }
@@ -25,7 +50,6 @@ table { border-collapse: collapse; border-spacing: 0; table-layout: fixed; }
 ol, ul { list-style: none outside none; }
 h1, h2, h3, h4, h5, h6 { font-size: 100%; font-weight: bold; }
 em, strong { font-style: normal; font-weight: bold; }
-html { font-family: PingFangSC-Light,helvetica,"Helvetica Neue", STHeiTi, "Microsoft YaHei", sans-serif !important; -webkit-text-size-adjust: 100%; text-size-adjust: 100%; font-size: 366.211%; }
 body, textarea, select, input { color: #000; }
 a, button { cursor: pointer; text-decoration: none; }
 body { position: relative; }
@@ -39,4 +63,7 @@ html { font-size: 14px; }
 :-ms-input-placeholder { color: #aaa; }
 input:-moz-placeholder { color: #aaa; }
 *:not(input) { -webkit-user-select: none; }
+.f-shadow {
+  text-shadow: 0 0 20px rgba(0,0,0,.6);
+}
 </style>
