b4daf8fce4415d106706056d1c95ef283eac9924
pengfei.yang <pengfei.yang@rokid.com>
2017-10-17 16:03:26 +0800

    fix: 修复bug

--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    // assetsPublicPath: 'http://www.hducc.top/ljj/display/',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'http://www.hducc.top/ljj/display/',
+    // assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
